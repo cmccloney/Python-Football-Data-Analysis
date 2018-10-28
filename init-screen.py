038aec727129df6c8899a71e7c980afb62bfd161
Conner McCloney <cmccloney@hotmail.com>
2018-10-28 15:33:47 -0600

    Update init-screen.py

--- init-screen.py
+++ init-screen.py
@@ -1,1 +1,14 @@
+from tkinter import *
+import os
 
+def PickScriptTkinter():
+    os.system("nfl-tkinter.py")
+
+def PickScriptDash():
+    os.system("nfl-dash.py")
+
+root=Tk()
+Label(root, text='Tkinter - Web-Scraped Information and Simple Graph').pack()
+Button(root, text='Tkinter', command=lambda:PickScriptTkinter()).pack()
+Label(root, text='Dash - WIP').pack()
+Button(root, text='Dash', command=lambda:PickScriptDash()).pack()

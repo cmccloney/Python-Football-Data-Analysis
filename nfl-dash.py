d1fe5fb61024b003082f37b875af7ce473855f19
Conner McCloney <cmccloney@hotmail.com>
2018-10-30 21:17:07 -0600

    Update nfl-dash.py

--- nfl-dash.py
+++ nfl-dash.py
@@ -1,1 +1,21 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
 
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div(children=[
+    html.H1(children='Work in Progress'),
+
+    html.Div(children='''
+        Will be updated in the future, just getting started.
+        -Need to add datasets into graphs, stats tests, etc.
+    '''),
+
+    
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
